4771c096e3b1b36649f2c48577f0db8bd50ff1e0
Gian Carlo Torres <geanu@outlook.com>
2023-05-07 21:45:48 -0400

    Functions and Procedures commit

--- cardeal-functions-procedures.sql
+++ cardeal-functions-procedures.sql
@@ -1,13 +1,108 @@
 -- FUNCTIONS and PROCEDURES
 
--- FUNCTION --
+-- FUNCTIONS --
+
+-- Insert Cars
+
+SELECT *
+FROM car;
+
+CREATE OR REPLACE FUNCTION insert_car (
+	_car_make_id INTEGER,
+	_vin VARCHAR,
+	_year INTEGER,
+	_color VARCHAR
+)
+RETURNS void AS $$
+	BEGIN
+		INSERT INTO car (
+			car_make_id,
+			vin,
+			year,
+			color
+		) VALUES (
+			_car_make_id,
+			_vin,
+			_year,
+			_color
+		)
+	END;
+$$
+LANGUAGE plpgsql;
+
+INSERT INTO car (
+	car_make_id,
+	vin,
+	year,
+	color
+) VALUES (
+	1,
+	'4Y1SL65848Z411450',
+	2020,
+	'slate'
+), (
+	5,
+	'4Z1SL65848Z411455',
+	2021,
+	'honey'
+), (
+	6,
+	'4A1SL65848Z411460',
+	2022,
+	'cinnabar'
+);
+
+SELECT insert_car(1,'4Y1SL65848Z411450',2020,'slate');
+SELECT insert_car(5,'4Z1SL65848Z411455',2021,'honey');
+SELECT insert_car(6,'4A1SL65848Z411460',2022,'cinnabar');
+
+SELECT *
+FROM car;
+
+-- Insert Address
+
+SELECT *
+FROM address;
+
+CREATE OR REPLACE FUNCTION add_address (
+	_address1 VARCHAR,
+	_address2 VARCHAR,
+	_district VARCHAR,
+	_city VARCHAR,
+	_postal_code INTEGER
+)
+RETURNS void AS $$
+	BEGIN
+		INSERT INTO car (
+			address1,
+			address2,
+			district,
+			city,
+			postal_code
+		) VALUES (
+			_address1,
+			_address2,
+			_district,
+			_city,
+			_postal_code
+		)
+	END;
+$$
+LANGUAGE plpgsql;
+
+SELECT add_address('150 Jay St', 'Apt 2B', 'Brooklyn', 'New York City', 10321);
+SELECT add_address('79 Carol Ave', '', 'Manhattan', 'New York City', 10101);
+SELECT add_address('1334 Lars Way', 'Apt 98', 'Queens', 'New York City', 10221);
+
+SELECT *
+FROM address;
 
 -- Change the Sales Person assigned to a Car Sale
 
 SELECT *
 FROM car_sale;
 
-CREATE OR REPLACE FUNCTION update_sales_person(
+CREATE OR REPLACE FUNCTION update_sales_person (
 	_car_sale_id INTEGER,
 	_sales_id INTEGER
 )
@@ -25,11 +120,9 @@ FROM car_sale;
 
 SELECT update_sales_person(2, 1);
 
-
--- PROCEDURE --
+-- PROCEDURES --
 
 -- Assign Mechanic to a Service Team
-
 
 SELECT *
 FROM service_team;
